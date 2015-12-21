67c93a542e38273d8f2ead02abf09dd8aec6ff0f
Jens Holm <jensgholm15@gmail.com>
2015-12-21 13:56:24 +0100

    hmm

--- implementation/DataCollection/runner.py
+++ implementation/DataCollection/runner.py
@@ -1,6 +1,10 @@
 import threading
 import Queue
 import os
+import sys
+import re
+from random import randint
+from time import sleep
 
 tasks = Queue.Queue()
 
@@ -14,26 +18,55 @@ def work():
   while not tasks.empty():
     cmd = tasks.get()
     print cmd
+    sleep(randint(1,10))
     os.system( cmd )
 
 def main():
-  
-  
-  for i in range(0, 10):
-    tasks.put( "./ExampleProject -optimizer=ce -noiseType=0 -noise=0 -maxiter=81 -sigma=100 -output=ce_NoNoise_%d" % i )
 
-  for i in range(0, 10):
-    tasks.put( "./ExampleProject -optimizer=ce -noiseType=1 -noise=4 -maxiter=81 -sigma=100 -output=ce_ConstantNoise_%d" % i )
+  if len(sys.argv) < 3:
+    print "Usage: python runner.py {experiment file} {number of concurrent processes}"
+    exit()
 
-  for i in range(0, 10):
-    tasks.put( "./ExampleProject -optimizer=ce -noiseType=2 -noise=4 -maxiter=81 -sigma=100 -output=ce_LinearNoise_%d" % i )
+  if not os.path.isfile(sys.argv[1]):
+    print "First argument is not a filepath"
+    exit()
 
-  nThreads = 4
+  if not sys.argv[2].isdigit():
+    print "Second argument must be an integer"
+    exit()
+
+  lineIt = 0
+  reg = re.compile("^(\(.*\)) x (\d+)")
+  hasIterator = re.compile(".*(\$i).*")
+  with open(sys.argv[1], 'r') as f:
+    for line in f:
+      lineIt += 1
+      m = reg.match(line)
+      repNum = m.group(2)
+      expression = m.group(1)
+      if not m:
+        print "Expression on line %d did not compile." % lineIt
+        exit()
+      if not hasIterator.match(expression):
+        print "Expression on line %d did not have an iterator." % lineIt
+      if not repNum.isdigit():
+        print "Expression on line %d has ill formatted repetition number: %s" % (lineIt, repNum)
+
+      for i in range(0, int(repNum)):
+        proc = expression.replace("$i", str(i))
+        print proc
+        tasks.put( proc  )
+      
+        
+
+  nThreads = int(sys.argv[2])
   threads = []
   for i in range(0, nThreads):
     t = threading.Thread(target=work, args=[])
+    t.daemon = True
     threads.append( t )
     t.start()
+    sleep(randint(1,10))
   
 
   for i in range(0, nThreads):
