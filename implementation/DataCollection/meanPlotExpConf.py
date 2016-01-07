fff3c0d83d9ed8f652d1aa5fd8e567946f610293
Jens Holm <jensgholm15@gmail.com>
2016-01-07 21:13:01 +0100

    add: mean plot

--- implementation/DataCollection/meanPlotExpConf.py
+++ implementation/DataCollection/meanPlotExpConf.py
@@ -73,7 +73,7 @@ def main():
   meanData = {}
   observations = -1
   largestRefPoint = 0
-  for i in range(0, 80000):
+  for i in range(0, 800000):
     if i in sumData:
       largestRefPoint = i
       if observations == -1:
@@ -88,10 +88,12 @@ def main():
   # Check if the output file exists
   if os.path.isfile(outputFile):
     exit("output file already exists")
+    
+  generationData = {}
 
   # write the CSV data to the file
   with open(outputFile, 'w') as csvfile:
-    fieldnames = [refCol, meanCol, 'clow', 'chigh']
+    fieldnames = [refCol, meanCol, 'clow', 'chigh', 'Q1', 'Q2', 'Q3', 'means']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
     for i in range(0, 80000):
@@ -101,10 +103,40 @@ def main():
         rateUpp = rateHat * (1 + ( 1.96 / math.sqrt(30) ) )
         clow  = 1.0 / rateLow
         chigh = 1.0 / rateUpp
-        writer.writerow( { refCol:i , meanCol:meanData[i], 'clow':clow, 'chigh':chigh } )
+        
+        sortedMeans = sorted(sumData[i])
+        
+        # Define quantiles and calculate
+        Q1 = 0
+        Q2 = 0
+        Q3 = 0
+        n = len(sortedMeans)
+        Q1 = (sortedMeans[int(math.floor(n   / 4.0) ) -1 ] + sortedMeans[int(math.ceil(n   / 4.0) ) - 1  ]) / 2
+        Q3 = (sortedMeans[int(math.floor(n*3.0 / 4.0) )  ] + sortedMeans[int(math.ceil(n*3.0 / 4.0) )  ]) / 2
+        
+        if (n % 2) == 0:
+            Q2 = (sortedMeans[int(n / 2)-1  ] + sortedMeans[int(n / 2)  ]) / 2
+        else:
+            Q2 = sortedMeans[int(math.floor(n/2))]
+        
+        writer.writerow( { refCol:i , meanCol:meanData[i], 'clow':clow, 'chigh':chigh, 'Q1':Q1, 'Q2':Q2, 'Q3':Q3, 'means':sortedMeans } )
+        
+        generationData[refCol] = i
+        generationData[meanCol] = meanData[i]
+        generationData['clow'] = clow
+        generationData['chigh'] = chigh
+        generationData['Q1'] = Q1
+        generationData['Q2'] = Q2
+        generationData['Q3'] = Q3
         
     # Report data
     print "Last x-axis point: %s" % str(largestRefPoint)
+    
+    # Prting last iteration values
+    print generationData
+    
+   
+    
 
 
 if __name__ == "__main__":
